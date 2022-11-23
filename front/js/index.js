7699f4d2bd2cca9468a1791b53a3c2ec6ad805b7
LucileBch <l.bouchire@gmail.com>
2022-11-23 10:22:23 +0100

    intégration de l'ensemble des produits sur la page d'accueil

---
+++ front/js/index.js
@@ -0,0 +1,25 @@
+// Appel à l'API qui contient les données avec une requête GET
+async function getDatas(){
+    const res = await fetch('http://localhost:3000/api/products');
+    // Spécification du format attendu pour les résultats
+    const datas = await res.json();
+
+    // Création d'une boucle FOR...OF pour sélectionner quelles informations, pour chacun des produits, sont à afficher en les appelant via ${}
+    for(let data of datas){
+        const product =`<a href="./product.html?id=${data._id}">
+        <article>
+          <img src="${data.imageUrl}" alt="${data.altText}">
+          <h3 class="productName">${data.name}</h3>
+          <p class="productDescription">${data.description}</p>
+        </article>
+      </a>`
+    
+        // Déclaration d'une variable pour pointer dans quel élément on souhaite afficher les produits
+        const section = document.querySelector('#items')
+        //Précision de la position dans l'élément (à l'intérieur de l'élement, juste après son dernier enfant) et appel à la fonction product pour dire ce qui doit être affiché
+        section.insertAdjacentHTML('beforeend', product)
+    }
+}
+
+// Lancement de la fonction
+getDatas()
