ab36f778152cb90b1318fce65dc7d3758a5b9530
LucileBch <l.bouchire@gmail.com>
2022-11-30 10:38:47 +0100

    Simplification affichage produit et commentaires

--- front/js/product.js
+++ front/js/product.js
@@ -1,65 +1,47 @@
-//consignes :
-// récupération des informations d'un produit
-// récupérer l'id dans url (searchparams)
-// récupérer les données du produit unique dans l'api
-// afficher les données du produit sur la page produit
-
-// Variables globales
+//---------------  AFFICHAGE PRODUIT ---------------
+// Récupération de l'Id du produit dans l'URL
 const urlSearchParams = new URLSearchParams(document.location.search)
 const itemId = urlSearchParams.get('id')
-console.log(itemId)
 
-const pageTitle = document.querySelector('title')
-const itemImage = document.querySelector('.item__img')
-const itemName = document.querySelector('#title')
-const itemPrice = document.querySelector('#price')
-const itemDescription = document.querySelector('#description')
-const itemColors = document.querySelector('#colors')
+// Récupération des informations uniques d'un produit en appelant les données de l'API
+async function getDatas(){
+    const res = await fetch('http://localhost:3000/api/products/' + itemId)
+    const data = await res.json()
 
-// je fais appel à l'API avec une requête GET pour récupérer les données produits via l'ID
-fetch (`http://localhost:3000/api/products/${itemId}`)
-    
-    // avec la fonction then(), je récupère le résultat de la requête au format json et vérifie que la requête s'est bien passée grâce à res.ok
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
+    // Affichage des informations produits en pointant où l'on souhaite qu'elles apparaissent
+    document.querySelector('title').innerHTML = data.name
+    document.querySelector('.item__img').innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
+    document.querySelector('#title').innerHTML = data.name
+    document.querySelector('#price').innerHTML = data.price
+    document.querySelector('#description').innerHTML = data.description
 
-    // avec la 2ème fonction then(), je récupère les vraies valeurs des résultats et je modifie les variables pour leurs attribuer les données souhaitées
-    .then(data => {
-        pageTitle.innerText = `${data.name}`
-        itemImage.innerHTML = `<img src="${data.imageUrl}" alt="${data.altText}"></img>`
-        itemName.textContent = `${data.name}`
-        itemPrice.textContent = `${data.price}`
-        itemDescription.textContent = `${data.description}`
-    
-        // voir forEach ? object.values ?
-        data.colors.forEach((color) => {
-            let colorOption = document.createElement("option")
-            colorOption.innerHTML = `${color}`
-            colorOption.value = `${color}`
-            itemColors.appendChild(colorOption)
-            console.log(itemColors)
-        });
-    })
-
-    // appel de la fonction catch() si une erreur survient lors de la requête
-    .catch (error => {
-        alert('Le serveur ne répond pas')
-    })
-
-
-
-function onButtonClick (event) {
-    //écrire l'action qui se passe au clic ++ préciser ici les conditions
-    console.log('button click')
+    // Récupération des options couleurs grâce à la boucle FOR...OF
+    for(option of data.colors){
+        const color = `<option value="${option}">${option}</option>`
+        document.querySelector('#colors').insertAdjacentHTML('beforeend', color)
+    }
 }
+// Lancement de la fonction pour afficher le produit 
+getDatas()
 
-const button = document.querySelector('#addToCart')
-button.addEventListener('click', onButtonClick) //appel de la fonction qui définit l'action qui se passe au clic
+//--------------- GESTION DU PANIER ---------------
 
-// si quantité < 1 ou > 100 et couleur non sélectionnée ==> on ajoute pas au panier
+document.querySelector('#addToCart').addEventListener('click', () =>{
+    Je récupère la valeur du input color
+    je récupère la valeur de la quantité
 
 
+    tester la couleur pour voir si vide, si vide mettre une alerte pour que l'utilisateur choisisse une couleur
 
+    tester si quantité inférieure a 1 ou sup à 100 = mettre une alerte
+
+    Si la couleur et la qet valide, lancer une fonction addToCart
+})
+
+//--------------- LOCAL STORAGE ---------------
+
+Pour l'ajout au panier (localstorage)
+
+Si un produit existe déjà = addition de la nouvelle qte a la qte déjà présente
+Attention, il ne doit pas être possible d'avoir plus de 100 produit identique et pas de valeur négative
+NE PAS STOCKER LE PRIX DANS LE localStorage
