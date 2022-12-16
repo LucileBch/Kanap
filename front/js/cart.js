2a8520b0088e025a3c8814a9990980746854c5b7
LucileBch <l.bouchire@gmail.com>
2022-12-16 17:20:40 +0100

    fonction total, modification de quantité et suppression produit MAJ

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,17 +1,24 @@
 let cart = JSON.parse(localStorage.getItem('clientItem'))
 
+//si produits dans le panier, appel fonction affichage - si non msg panier vide
 if (cart) {
     getDatas()
 } else {
     document.querySelector('#cartAndFormContainer > h1').textContent = 'Votre panier est vide'
 }
 
+//fonction appel données API
 async function getDatas() {
     const resp = await fetch('http://localhost:3000/api/products')
     const products = await resp.json()
+
+    //appel fonction affichage produits du panier
     displayCart(products)
+    modifyQuantity()
+    deleteItem()
 }
 
+//fonction affichage produits du panier
 function displayCart(products) {
     for (let element of cart) {
         const product = products.find(p => p._id === element.id)
@@ -41,4 +48,54 @@ function displayCart(products) {
 
         document.querySelector('#cart__items').insertAdjacentHTML('beforeend', display)
     }
+    //appel fonction total prix et quantié
+    total(products)
 }
+
+//fonction total prix et quantité
+function total(products) {
+    const displayTotalQuantity = document.querySelector('#totalQuantity')
+    let total = 0
+    for(let cartProduct of cart) {
+        total += cartProduct.quantity
+    }
+    displayTotalQuantity.innerText = total
+
+    const displayTotalPrice = document.querySelector('#totalPrice')
+    let totalPrice = 0
+    for(let cartProduct of cart) {
+        let quantity = cartProduct.quantity
+        const product = products.find(p => p._id === cartProduct.id)
+        let price = product.price
+        totalPrice += price * quantity
+    }
+    displayTotalPrice.innerText = totalPrice
+}
+
+// fonction modification quantité
+function modifyQuantity() {
+    let itemQuantities = document.querySelectorAll('.itemQuantity')
+
+    itemQuantities.forEach(itemQuantity => {
+        itemQuantity.addEventListener('change', (event) => {            
+            if(itemQuantity.value < 1 || itemQuantity.value > 100) {
+                alert('La quantité sélectionnée doit être comprise entre 1 et 100')
+            } else {
+                // appeler la fonction update cart
+            }
+        })
+    })
+}
+
+// fonction suppression produit
+function deleteItem() {
+    let deleteButtons = document.querySelectorAll('.deleteItem')
+
+    deleteButtons.forEach(deleteButton => {
+        deleteButton.addEventListener('click', (event) => {
+            alert('Le produit a été supprimé du panier')
+            // appel de la fonction update cart
+        })
+    })
+}
+
